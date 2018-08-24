5c205c51aed26aeff13e1f0795444d94e5150e20
afeiship <1290657123@qq.com>
2018-08-24 16:48:58 +0800

    issue: update

--- src/next-toggle.js
+++ src/next-toggle.js
@@ -1,15 +1,29 @@
 (function () {
 
-  var global = global || this || self || window;
+  var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var UNDEFIND = 'undefined';
 
-  nx.toggle = function (inArray, inItem) {
-    var idx = inArray.indexOf(inItem);
-    if(idx !== -1) {
+  nx.toggle = function (inArray, inItem, inPath) {
+    //inItem is a callback:
+    var array = inArray;
+    var value = inItem;
+    var idx = -1;
+
+    if (typeof inPath !== UNDEFIND) {
+      value = nx.path(inItem, inPath);
+      array = nx.map(inArray, function (_, item) {
+        return nx.path(item, inPath);
+      });
+    }
+
+    idx = array.indexOf(value);
+    if (idx !== -1) {
       inArray.splice(idx, 1);
     } else {
       inArray.push(inItem);
     }
+
     return inArray;
   };
 
--- test/test.js
+++ test/test.js
@@ -5,12 +5,30 @@ require('../src/next-toggle');
 describe('next/toggle', function () {
 
   it('toggle pure array', function () {
-    var arr1 = [1,2,3,4,5];
+    var arr1 = [1, 2, 3, 4, 5];
 
-    assert.deepEqual( nx.toggle(arr1, 5), [1,2,3,4]);
-    assert.deepEqual( nx.toggle(arr1, 5), [1,2,3,4,5]);
-    assert.deepEqual( nx.toggle(arr1, 5), [1,2,3,4]);
-    assert.deepEqual( nx.toggle(arr1, 5), [1,2,3,4,5]);
+    assert.deepEqual(nx.toggle(arr1, 5), [1, 2, 3, 4]);
+    assert.deepEqual(nx.toggle(arr1, 5), [1, 2, 3, 4, 5]);
+    assert.deepEqual(nx.toggle(arr1, 5), [1, 2, 3, 4]);
+    assert.deepEqual(nx.toggle(arr1, 5), [1, 2, 3, 4, 5]);
+  });
+
+  it('toggle by array  innner object', function () {
+    var items = [
+      { value: 'deposit', label: '保证金帐户', price: '2000' },
+      { value: 'extra', label: '使用余额', price: '1000' },
+      { value: 'de1', label: '使用余额xxx', price: '1000' },
+      { value: 'de2', label: '使用余额yyy', price: '1000' },
+    ];
+
+    var rs1 = nx.toggle(items, { value: 'de1', label: '使用余额xxx', price: '1000' }, 'value');
+
+    assert.deepEqual(rs1, [
+      { value: 'deposit', label: '保证金帐户', price: '2000' },
+      { value: 'extra', label: '使用余额', price: '1000' },
+      { value: 'de2', label: '使用余额yyy', price: '1000' },
+    ])
+
   });
 
 });
